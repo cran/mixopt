88288fab3c204996be05fea634dafb52dcd31abd
Collin Erickson <collinberickson@gmail.com>
2022-11-19 16:40:02 +0000

    version 0.1.1

--- NEWS.md
+++ NEWS.md
@@ -1,5 +1,11 @@
+# mixopt 0.1.1
+
+* Fixed two breaking errors in `index_line_search`.
+
 # mixopt 0.1.0
 
 * Created package
 
 * Added a `NEWS.md` file to track changes to the package.
+
+* Accepted to CRAN on 11/11/2022.
--- tests/testthat/test_coorddesc.R
+++ tests/testthat/test_coorddesc.R
@@ -67,3 +67,31 @@ test_that("coorddesc unordered", {
   expect_equal(cdout$par[[1]], 5)
   expect_equal(cdout$val, .16)
 })
+
+
+test_that("coorddesc cts and un", {
+  expect_error(
+    {
+      capture.output({
+        cdout <- mixopt_coorddesc(par=list(mopar_cts(5,15),
+                                           mopar_unordered(2:8, start=2)),
+                                  fn=function(x) {
+                                    (x[[1]] - 11.1) ^ 2 * (x[[2]] - 5.4) ^ 2 +
+                                      abs(x[[1]] - 11.1) + abs(x[[2]] - 5.4)
+                                  }, verbose=1e5)
+      })
+    }, NA
+  )
+  expect_equal(length(cdout$par), 2)
+  expect_equal(cdout$par[[1]], 11.1)
+  expect_equal(cdout$par[[2]], 5)
+  expect_equal(cdout$val, .4)
+})
+
+test_that("stopifnot", {
+  expect_error({
+    cdout <- mixopt_coorddesc(par=list(mopar_cts(2,8)),
+                              fn=function(x) {(4.5-x[[1]])^2},
+                              maxeval = .3)
+  })
+})
